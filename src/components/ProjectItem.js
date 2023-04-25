231381f3cd6ce22dbd7dfb43546c3d147d91b8b1
Mark Wanjau <theafrikanson@gmail.com>
2023-04-25 03:35:21 +0300

    add comment on projectitem.js

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,6 +7,11 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {/* The div tag with the class name of "technologies" maps over the technologies 
+        array and renders each technology in a span tag with a unique key. */} 
+        {/* The map function iterates over the array and returns a new array with the span tags,
+         each containing a technology from the technologies array.*/}
+
         {technologies.map((tech) => {
 
           return <span key={tech}>{tech}</span>;
