d27edf2fd5faf1bf6c40f969528c0a6ceb117203
Mark Wanjau <theafrikanson@gmail.com>
2023-04-25 03:57:38 +0300

    pass projectlist.js test

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,33 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+        {/*The projects array is passed as a prop to the component, and it contains a list of project objects,
+         each with properties such as name, about, and technologies.
+          The map function is used to iterate over the projects array and return a new array of ProjectItem components,
+          one for each project in the array. The key prop is set to the id property of the project object,
+           which ensures that each ProjectItem component has a unique identifier. 
+           The name, about, and technologies properties of each project object are passed as props to the corresponding ProjectItem component.
+          The resulting list of ProjectItem components is rendered inside the div with the id attribute set to "project-list".
+        */}
+            {projects.map((project) => {
+              return (
+
+                    <ProjectItem
+                        key={project.id}
+                        name={project.name}
+                        about={project.about}
+                        technologies={project.technologies}
+                    />    
+
+              );
+
+            })}
+        
+        
+        
+      </div>
     </div>
   );
 }
